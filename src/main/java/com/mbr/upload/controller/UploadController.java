1383181c6b51014fd34ab10db0a2d507d982cc54
罗庆彬 <361503082@qq.com>
2018-05-21 14:13:35 +0800

    修改配置

--- src/main/java/com/mbr/upload/controller/UploadController.java
+++ src/main/java/com/mbr/upload/controller/UploadController.java
@@ -5,6 +5,7 @@ import com.mbr.upload.common.controller.BaseController;
 import com.mbr.upload.common.utils.TimestampPkGenerator;
 import com.mbr.upload.domain.Upload;
 import com.mbr.upload.manager.UploadManager;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -17,8 +18,8 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -32,15 +33,15 @@ public class UploadController extends BaseController<Upload> {
     @Autowired
     private UploadManager uploadManager;
 
-    @RequestMapping(value = "upload",method = RequestMethod.POST)
+    @RequestMapping(value = "upload", method = RequestMethod.POST)
     @ResponseBody
-    public Object upload(HttpServletRequest request){
-        Map<String,String> map = new HashMap<>();
+    public Object upload(HttpServletRequest request) {
+        Map<String, String> map = new HashMap<>();
         CommonsMultipartResolver cmr = new CommonsMultipartResolver(request.getServletContext());
         if (cmr.isMultipart(request)) {
             MultipartHttpServletRequest mRequest = (MultipartHttpServletRequest) (request);
             Map<String, MultipartFile> mapFiles = mRequest.getFileMap();
-            for(Map.Entry<String, MultipartFile> entry : mapFiles.entrySet()) {
+            for (Map.Entry<String, MultipartFile> entry : mapFiles.entrySet()) {
                 MultipartFile file = entry.getValue();
                 if (file != null) {
                     Upload upload = new Upload();
@@ -52,22 +53,26 @@ public class UploadController extends BaseController<Upload> {
                     String type = fileName.substring(lastIndex + 1, fileName.length());
                     fileName = UUID.randomUUID().toString().replace("-", "") + "." + type;
                     upload.setContentType(type);
-                    upload.setSize(fileName.length() + "");
+                    try {
+                        upload.setSize(String.valueOf(file.getInputStream().available()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     upload.setFileName(fileName);
                     String newImageDisk = imageDisk;
                     String date = simpleDateFormat.format(new Date());
-                    newImageDisk = newImageDisk+"/"+date;//目录
+                    newImageDisk = newImageDisk + "/" + date;//目录
                     File f = new File(newImageDisk);
-                    if(!f.isDirectory()){
+                    if (!f.isDirectory()) {
                         f.mkdirs();
                     }
-                    String filePath = newImageDisk+"/"+fileName;
+                    String filePath = newImageDisk + "/" + fileName;
                     upload.setDiskUrl(filePath);
                     upload.setInternetUrl("/download/" + upload.getId());
                     try {
                         file.transferTo(new File(filePath));
                         uploadManager.save(upload);
-                        map.put(entry.getKey(),upload.getInternetUrl());
+                        map.put(entry.getKey(), upload.getInternetUrl());
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -76,6 +81,43 @@ public class UploadController extends BaseController<Upload> {
         }
         return success(map);
 
+    }
+
+    @RequestMapping(value = "uploadForPlist", method = RequestMethod.POST)
+    @ResponseBody
+    public Object uploadForPlist(@RequestParam(value = "data")String data) {
+        Map<String, String> map = new HashMap<>();
+        Upload upload = new Upload();
+        upload.setId(new TimestampPkGenerator().next(getClass()));
+        upload.setCreateTime(new Date());
+
+        String fileName = "ios.plist";
+        int lastIndex = fileName.lastIndexOf(".");
+        String type = fileName.substring(lastIndex + 1, fileName.length());
+        fileName = UUID.randomUUID().toString().replace("-", "") + "." + type;
+        upload.setContentType(type);
+        upload.setSize("");
+        upload.setFileName(fileName);
+        String newImageDisk = imageDisk;
+        String date = simpleDateFormat.format(new Date());
+        newImageDisk = newImageDisk + "/" + date;//目录
+        File f = new File(newImageDisk);
+        if (!f.isDirectory()) {
+            f.mkdirs();
+        }
+        String filePath = newImageDisk + "/" + fileName;
+        upload.setDiskUrl(filePath);
+        upload.setInternetUrl("/download/" + upload.getId());
+        try {
+            //file.transferTo(new File(filePath));
+            OutputStream outputStream = new FileOutputStream(filePath);
+            IOUtils.write(data,outputStream,"UTF-8");
+            uploadManager.save(upload);
+            map.put("plist", upload.getInternetUrl());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return success(map);
     }
 
 }
