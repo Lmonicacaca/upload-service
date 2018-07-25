30e5d93c71a7d61a5bd66e8af3f9f6a1b0dcb111
刘建成 <liujc@cqmbr.net>
2018-07-25 19:07:06 +0800

    端口与profile动态配置

--- src/main/java/com/mbr/upload/UploadApplication.java
+++ src/main/java/com/mbr/upload/UploadApplication.java
@@ -12,7 +12,20 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class UploadApplication {
 
+
     public static void main(String[] args) {
-        SpringApplication.run(UploadApplication.class, args);
+        if (args == null || args.length == 0) {
+            String[] args_ = {"--spring.profiles.active=dev", "--spring.cloud.config.profile=dev"};
+            System.out.println("===================================================================");
+            System.out.println("===================================================================");
+            System.out.println("=======@@@@@启动参数不能为空，默认将以开发环境启动@@@@@============");
+            System.out.println("============如需启动其它环境，请参考至少添加以下两个参数：=========");
+            System.out.println("--spring.profiles.active=dev --spring.cloud.config.profile=dev     ");
+            System.out.println("===================================================================");
+            System.out.println("===================================================================");
+            SpringApplication.run(UploadApplication.class, args_);
+        } else {
+            SpringApplication.run(UploadApplication.class, args);
+        }
     }
 }
